c46c5d96f6dda82f666591a00239fa899099a5d9
Omar N <54334118+omnz@users.noreply.github.com>
2019-11-26 10:45:28 -0500

    Add files via upload

--- Rock Paper Scissors/game.js
+++ Rock Paper Scissors/game.js
@@ -1,5 +1,18 @@
+const pPoints = document.querySelector('#playerPoints');
+const cPoints = document.querySelector('#computerPoints');
+const outcome = document.querySelector('#outcome');
+const pChoice = document.querySelector('#pChoice');
+const cChoice = document.querySelector('#cChoice');
+const victory = document.querySelector('#victory');
+const turnsElement = document.querySelector('#turns');
+
 let playerPoints = 0;
 let computerPoints = 0;
+let turns = 5;
+
+pPoints.className = 'text';
+cPoints.className = 'text';
+turnsElement.innerHTML = turns;
 
 function computerPlay() {
   const choices = ['Rock', 'Paper', 'Scissors'];
@@ -11,57 +24,75 @@ function playRound(playerSelection, computerSelection) {
   let msg = '';
   playerSelection = playerSelection.toLowerCase();
 
+  // Check who won round
   if (playerSelection == computerSelection.toLowerCase()) {
     msg = "Tie!";
   }
   else if (playerSelection == 'rock' && computerSelection == 'Paper') {
-    msg = "You Lose! Paper beats Rock.";
+    msg = "Rock < Paper";
     computerPoints++;
+    turns--;
   }
   else if (playerSelection == 'rock' && computerSelection == 'Scissors') {
-    msg = "You Win! Rock beats Scissors.";
+    msg = "Rock > Scissors.";
     playerPoints++;
+    turns--;
   }
   else if (playerSelection == 'paper' && computerSelection == 'Rock') {
-    msg = "You Win! Paper beats Rock.";
+    msg = "Paper > Rock";
     playerPoints++;
+    turns--;
   }
   else if (playerSelection == 'paper' && computerSelection == 'Scissors') {
-    msg = "You Lose! Scissors beats Paper.";
+    msg = "Paper < Scissors";
     computerPoints++;
+    turns--;
   }
   else if (playerSelection == 'scissors' && computerSelection == 'Rock') {
-    msg = "You Lose! Rock beats Scissors.";
+    msg = "Scissors < Rock";
     computerPoints++;
+    turns--;
   }
   else if (playerSelection == 'scissors' && computerSelection == 'Paper') {
-    msg = "You Win! Scissors beats Paper.";
+    msg = "Scissors > Paper";
     playerPoints++;
+    turns--;
   }
-  console.log(msg);
+
+  // Update turn
+  turnsElement.innerHTML = turns;
+
+  // Update turn outcome
+  outcome.innerHTML = msg;
+
   return (playerPoints, computerPoints);
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, Paper, or Scissors?");
+function game(playerChoice) {
+  if (turns != 0) {
     const computerSelection = computerPlay();
 
-    playRound(playerSelection, computerSelection);
-  }
+    // Update player and computer choice
+    pChoice.innerHTML = playerChoice;
+    cChoice.innerHTML = computerSelection;
 
-  console.log("Player points: " + playerPoints);
-  console.log("Computer points: " + computerPoints);
+    playRound(playerChoice, computerSelection);
 
-  if (playerPoints > computerPoints) {
-    console.log("You won the game!");
-  }
-  else if (playerPoints < computerPoints) {
-    console.log("You lost the game!");
-  }
-  else if (playerPoints > computerPoints) {
-    console.log("You tied against computer!");
+    // Update current points
+    pPoints.innerHTML = playerPoints;
+    cPoints.innerHTML = computerPoints;
+
+    // Check who won the game
+    if (turns == 0) {
+      if (playerPoints > computerPoints) {
+        victory.innerHTML = "You won the game!";
+      }
+      else if (playerPoints < computerPoints) {
+        victory.innerHTML = "You lost the game!";
+      }
+      else if (playerPoints == computerPoints) {
+        victory.innerHTML = "You tied against computer!";
+      }
+    }
   }
 }
-
-game();
